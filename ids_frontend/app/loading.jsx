ce625f9dde2f7ba7951bf2019cc6e8e742d3dbbe
Likith D <likithsurya555@gmail.com>
2026-05-30 23:09:34 +0530

    Restore animated Loader component

--- ids_frontend/app/loading.jsx
+++ ids_frontend/app/loading.jsx
@@ -1,7 +1,5 @@
+import Loader from "../components/Loader/Loader";
+
 export default function Loading() {
-  return (
-    <div className="flex items-center justify-center min-h-[50vh] text-gray-500 dark:text-gray-400 font-mono text-xs tracking-widest uppercase">
-      Loading...
-    </div>
-  );
+  return <Loader />;
 }
